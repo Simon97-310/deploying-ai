cba0d3b271e37ebcdfcd196f50217011ec533cb9
Simon Meng <simon.meng97@gmail.com>
2026-03-01 21:19:51 -0500

    My assignment-2. Wasn't able to complete but I tried

---
+++ 05_src/assignment_chat/main.py
@@ -0,0 +1,54 @@
+from langgraph.graph import StateGraph, MessagesState, START
+from langchain.chat_models import init_chat_model
+from langgraph.prebuilt.tool_node import ToolNode, tools_condition
+from langchain_core.messages import SystemMessage,  HumanMessage
+
+from dotenv import load_dotenv
+import json
+import requests
+import os
+
+from assignment_chat.instructions import return_instructions
+from assignment_chat.tools_currency import get_ex_rate
+
+from utils.logger import get_logger
+
+
+_logs = get_logger(__name__)
+load_dotenv(".env")
+load_dotenv(".secrets")
+
+chat_agent = init_chat_model(
+    "openai:gpt-4o-mini",
+    temperature=0.7,
+    base_url='https://k7uffyg03f.execute-api.us-east-1.amazonaws.com/prod/openai/v1', 
+    api_key='any value',
+    default_headers={"x-api-key": os.getenv('API_GATEWAY_KEY')}
+)
+tools = [get_ex_rate]
+
+instructions = return_instructions()
+
+
+
+# @traceable(run_type="llm")
+def call_model(state: MessagesState):
+    """LLM decides whether to call a tool or not"""
+    response = chat_agent.bind_tools(tools).invoke( [SystemMessage(content=instructions)] + state["messages"])
+    return {
+        "messages": [response]
+    }
+
+def get_graph():
+    
+    builder = StateGraph(MessagesState)
+    builder.add_node(call_model)
+    builder.add_node(ToolNode(tools))
+    builder.add_edge(START, "call_model")
+    builder.add_conditional_edges(
+        "call_model",
+        tools_condition,
+    )
+    builder.add_edge("tools", "call_model")
+    graph = builder.compile()
+    return graph
